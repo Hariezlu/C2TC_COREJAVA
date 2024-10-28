abdbda1d0341f705b0d02135a7bcc114c345e02a
Hari <Hari@DESKTOP-L72S1VN>
2024-10-28 19:23:23 +0530

    WhileDemo

---
+++ C2TC_HARI/src/com/tns/ifet/day2/WhileDemo.java
@@ -0,0 +1,15 @@
+package com.tns.ifet.day2;
+
+public class WhileDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=98;
+		while(i<=100)
+		{
+		System.out.println(i);
+		i++;
+		}
+	}
+
+}
