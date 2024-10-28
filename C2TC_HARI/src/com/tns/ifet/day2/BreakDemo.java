9c9447012c3c0e0ba6b937eacb5dc80811a95bb7
Hari <Hari@DESKTOP-L72S1VN>
2024-10-28 19:05:49 +0530

    BreakDemo

---
+++ C2TC_HARI/src/com/tns/ifet/day2/BreakDemo.java
@@ -0,0 +1,14 @@
+package com.tns.ifet.day2;
+
+public class BreakDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 6; i < 100; i++) {
+			if (i == 15)
+			break;
+			System.out.println(i);
+		}
+	}
+
+}
