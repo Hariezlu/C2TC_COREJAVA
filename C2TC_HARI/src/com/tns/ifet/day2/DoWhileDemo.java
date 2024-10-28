0f3ec5723f0d4fe978299549b37d9167de979b69
Hari <Hari@DESKTOP-L72S1VN>
2024-10-28 19:25:13 +0530

    DoWhileDemo

---
+++ C2TC_HARI/src/com/tns/ifet/day2/DoWhileDemo.java
@@ -0,0 +1,15 @@
+package com.tns.ifet.day2;
+
+public class DoWhileDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 20;
+		do {
+		System.out.println(i);
+		i++;
+		} while (i <= 1);
+
+	}
+
+}
