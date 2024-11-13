098013c27d69613d9f9244cbc87c56ec503757cd
Hari <Hari@DESKTOP-L72S1VN>
2024-11-13 09:40:50 +0530

    day 3

---
+++ C2TC_HARI/src/com/pvr/shopping/impl_ordering/ComputerTools.java
@@ -0,0 +1,10 @@
+package com.pvr.shopping.impl_ordering;
+
+public class ComputerTools {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ C2TC_HARI/src/com/pvr/shopping/impl_ordering/MobileTools.java
@@ -0,0 +1,10 @@
+package com.pvr.shopping.impl_ordering;
+import com.pvr.shopping.interfacesample.Order;
+public class MobileTools implements Order {
+	//private static 
+	public void ordering()
+	{
+		System.out.println("i'm buying bluetooth headphone");
+	}
+
+}
